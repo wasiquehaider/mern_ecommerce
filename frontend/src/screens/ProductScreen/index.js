cd9ca7e657bc77b62e5fe518079432181e59b3bd
Wasique Haider <wasiquehaider02@gmail.com>
2021-05-23 05:13:13 +0500

    express api setup

--- frontend/src/screens/ProductScreen/index.js
+++ frontend/src/screens/ProductScreen/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import { Rating } from "../../components";
-import products from "../../products";
+import axios from "axios";
+
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/product/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
@@ -54,7 +62,7 @@ const ProductScreen = ({ match }) => {
                 <Button
                   className="btn-block"
                   type="button"
-                  disabled={product.countInStock == 0}
+                  disabled={product.countInStock === 0}
                 >
                   Add To Cart
                 </Button>
