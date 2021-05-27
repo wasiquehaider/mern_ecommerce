bf3a7ca41727a51acd19ca6d0852125bb063ff87
Wasique Haider <wasiquehaider02@gmail.com>
2021-05-28 03:38:39 +0500

    User authentication

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+router.post("/login", authUser);
+router.post("/", registerUser);
+router.route("/profile").get(protect, getUserProfile);
+export default router;
